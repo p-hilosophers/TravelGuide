9d04f10a33a2c617f28d8be65bb16c23f094f5cc
GeorKyrs <georkyrs@teiser.gr>
2018-11-11 23:56:41 +0200

    Login and Register forms with database communication

---
+++ app/src/main/java/com/hilosophers/p/travelguide/Activity/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.hilosophers.p.travelguide.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hilosophers.p.travelguide.EncryptService;
+import com.hilosophers.p.travelguide.Model.User;
+import com.hilosophers.p.travelguide.R;
+import com.hilosophers.p.travelguide.UserClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        setTitle("Login");
+
+        final EditText email = findViewById(R.id.editTextEmail);
+        final EditText password = findViewById(R.id.editTextPassword);
+        final Button login = findViewById(R.id.buttonLogin);
+        final TextView registerLink = findViewById(R.id.textViewRegisterHere);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!(email.getText().toString().equals("") || password.getText().toString().equals(""))) {
+                    Retrofit.Builder builder = new Retrofit.Builder().baseUrl("http://192.168.1.2:8181/").addConverterFactory(GsonConverterFactory.create());
+                    Retrofit retrofit = builder.build();
+                    UserClient client = retrofit.create(UserClient.class);
+                    Call<User> call = client.userLogin(email.getText().toString(), EncryptService.encryptPassword(password.getText().toString()));
+                    call.enqueue(new Callback<User>() {
+                        @Override
+                        public void onResponse(Call<User> call, Response<User> response) {
+                            Intent areaIntent = new Intent(LoginActivity.this, CityActivity.class);
+                            LoginActivity.this.startActivity(areaIntent);
+                        }
+
+                        @Override
+                        public void onFailure(Call<User> call, Throwable t) {
+                            Toast.makeText(LoginActivity.this, "The inserted e-mail or password is incorrect !", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }else{
+                    Toast.makeText(LoginActivity.this, "Please fill in your credentials .", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/hilosophers/p/travelguide/UserClient.java
@@ -0,0 +1,18 @@
+package com.hilosophers.p.travelguide;
+
+import com.hilosophers.p.travelguide.Model.User;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface UserClient {
+
+    @POST("users")
+    Call<User> createAccount(@Body User user);
+
+    @GET("users/{userEmail}/{password}")
+    Call<User> userLogin(@Path("userEmail") String userEmail, @Path("password") String password);
+}
